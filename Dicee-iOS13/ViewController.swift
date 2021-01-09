33c8b714d6ff9ed78318f15c6866f8228b518b21
Cabrera Navarro, Kevin Oswaldo <kfvf671@astrazeneca.net>
2021-01-09 09:40:26 -0600

    add second dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,16 +13,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var leftDiceNumber = 1;
+    var rightDiceNumber = 5;
+    var dices =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix");
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo");
     }
 
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button got tapped");
+        print(leftDiceNumber);
+        print(rightDiceNumber);
+        
+        diceImageView1.image = dices[leftDiceNumber];
+        diceImageView2.image = dices[rightDiceNumber];
+        
+        leftDiceNumber = (leftDiceNumber + 1) % 6;
+        rightDiceNumber = abs((rightDiceNumber - 1) % 6);
     }
 }
 
